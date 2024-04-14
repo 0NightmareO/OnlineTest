52de5f398bb34267b3f48de4ffdae4187056222a
NgoTa <tieuta12375@gmail.com>
2024-04-14 17:39:45 +0700

    Add2

---
+++ test dd/create quiz/create quiz.js
@@ -0,0 +1,9 @@
+// const slide = document.querySelector('.slide');
+// const iconPush = document.querySelector('.icon-push');
+
+// iconPush.addEventListener('click', () => {
+//     slide.classList.remove('active-push');
+// });
+// iconPush.addEventListener('click', () => {
+//     slide.classList.add('active-push');
+// });
